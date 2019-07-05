b66094b4f99ff0cf90422f8a5faf12f9e5493bd3
184021 <184021@ocs4134.lab-ocs.local>
2019-07-05 10:44:25 +0900

    gui

---
+++ src/gui/GUI.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class GUI extends JFrame{
+	public GUI(String title) {
+		super(title);
+		setLocation(500,400);
+		setSize(500,300);
+	}
+}

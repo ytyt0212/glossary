58babd3ea73a9fef2850d80f3fb24c6b565fcd54
ともや <@DESKTOP-G890NC8>
2019-07-12 23:57:23 +0900

    7/12 sql select文

--- src/sql/Sql.java
+++ src/sql/Sql.java
@@ -1,5 +1,56 @@
 package sql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Sql {
+	String sqlwords = null;
+	String sqltext = null;
+	String sqlCategory = null;
+
+	public String sql(String sqlStr) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/oop?serverTimezone=JST", "root",
+					"root");
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = null;
+
+			rs = stmt.executeQuery(sqlStr);
+
+			if (sqlStr.contains("select words")) {
+				sqlwords = rs.getString("words");
+			} else if (sqlStr.contains("select text")) {
+				sqltext = rs.getString("text");
+			} else if (sqlStr.contains("select category_name")) {
+				sqlCategory = rs.getString("category_name");
+			}
+
+			rs.close();
+			stmt.close();
+			con.close();
+
+			if (sqlStr.contains("select words")) {
+				return sqlwords;
+			} else if (sqlStr.contains("select text")) {
+				return sqltext;
+			} else if (sqlStr.contains("select cateogy_name")) {
+				return sqlCategory;
+			} else {
+				return "error";
+			}
+
+		} catch (ClassNotFoundException e) {
+			return "error";
+
+		} catch (Exception e) {
+			return "error";
+
+		}
+	}
 
 }
